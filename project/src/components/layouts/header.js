6af875f7c52f45c2c5f4075dfd7040d9ab6bb849
Yhujae <117284729+Yhujae@users.noreply.github.com>
2022-11-26 18:14:57 +0100

    i added a H1 tag

--- project/src/components/layouts/header.js
+++ project/src/components/layouts/header.js
@@ -1,1 +1,8 @@
-ahzkcjxb
+function Header() {
+  return (
+    <div>
+      <h1>HEADER</h1>
+      <p>Nduka bu ezigbo mmadu</p>
+    </div>
+  );
+}
---
+++ project/src/components/login.js
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div>
+      <h1>Issues</h1>
+      <label>Email</label>
+      <input type="text" placeholder="Email address" />
+      <button>Enter</button>
+    </div>
+  );
+}
+export default Login;
