1b2345dd9b88c8093ca25f4974ff595f6a90862c
Nduka Okengwu <danielonduka@gmail.com>
2022-11-29 10:01:58 +0100

    Modified the home page to show how to use the layout and place the children components (Carousel, Trends etc

--- project/src/components/home/home.js
+++ project/src/components/home/home.js
@@ -1,4 +1,12 @@
+import Layout from "../layouts/layout";
+
 function Home() {
-  //Todo: the home page would be built here. For now testing is still being done usung the App.js file
-  return <></>;
+  //Todo: the home page would be built here.
+  return (
+    <>
+      <Layout>
+        {/* The carousel trending topics and styles come in here */}
+      </Layout>
+    </>
+  );
 }
