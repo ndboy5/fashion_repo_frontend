1e23eb4635abc4020346a8601c5f42191dd63ab9
entity-creation <davidkings1604@gmail.com>
2022-12-02 02:42:50 -0800

    chat component update 1

--- project/src/components/chat/chatHeader.js
+++ project/src/components/chat/chatHeader.js
@@ -1,20 +1,23 @@
 import styles from './chatHeader.module.css';
-import {IoIosAdd, IoIosSearch} from 'react-icons/io';
+import {IoIosAdd, IoIosArrowBack, IoIosSearch} from 'react-icons/io';
 import { FaChevronDown, FaInfo } from 'react-icons/fa';
 function ChatHeader(){
     return(
         <div className={styles.container}>
             <button className={styles.btn}>
                 <i>
-                    <IoIosAdd/>
+                    <IoIosArrowBack/>
                 </i>
             </button>
 
             <div className={styles.title}>
-                <h3>Sample chat forum</h3>
-                <button>
+                <h3>
+                    Sample chat forum
+                    <button className={styles.headerIcon}>
                     <i><FaChevronDown/></i>
                 </button>
+                </h3>
+                
             </div>
 
             <button className={styles.btn}>
--- project/src/components/chat/chatMessage.js
+++ project/src/components/chat/chatMessage.js
@@ -1,5 +1,7 @@
 import styles from "./chatMessage.module.css";
-import {IoMdAddCircleOutline, IoMdSend} from 'react-icons/io';
+import {IoIosAddCircle, IoMdAddCircleOutline, IoMdSend} from 'react-icons/io';
+import {GrEmoji} from 'react-icons/gr';
+import {BiMicrophone} from 'react-icons/bi';
 function ChatMessage(){
     return(
         <div className={styles.container}>
@@ -9,12 +11,32 @@ function ChatMessage(){
             <div>
             <button className={styles.btn}>
                 <i>
-                    <IoMdAddCircleOutline/>
+                    <IoIosAddCircle/>
                 </i>
             </button>
             <button className={styles.btn}>
                 <i>
                     <IoMdSend/>
+                </i>
+            </button>
+            <button className={styles.btn}>
+                <i>
+                    <GrEmoji/>
+                </i>
+            </button>
+            <button className={styles.btn}>
+                <i>
+                    <div className={styles.textIcon}>@</div>
+                </i>
+            </button>
+            <button className={styles.btn}>
+                <i>
+                    <div className={styles.textIcon}>Aa</div>
+                </i>
+            </button>
+            <button className={styles.btn}>
+                <i>
+                    <BiMicrophone/>
                 </i>
             </button>
             </div>
