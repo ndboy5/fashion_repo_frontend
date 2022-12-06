13d7345952c187c90ce2d1ca93f03e9815a048c2
Yhujae <117284729+Yhujae@users.noreply.github.com>
2022-12-06 21:30:07 +0100

    adjusted gallery

--- project/src/components/gallery/gallery.js
+++ project/src/components/gallery/gallery.js
@@ -4,12 +4,29 @@ function Gallery() {
     <div className={styles.body}>
       <div className={styles.container}>
         <div className={styles.box1}>
+          <img src="/images/style-7.jpg"></img>
+        </div>
+        <div className={styles.box2}>
+          <img src="/images/style-1.jpg"></img>
+        </div>
+        <div className={styles.box3}>
+          <img src="/images/trend_style/pic_c.jpg"></img>
+        </div>
+        <div className={styles.box}>
+          <img src="/images/style-2.jpg"></img>
+        </div>
+        <div className={styles.box}>
+          <img src="/images/style-3.jpg"></img>
+        </div>
+        <div className={styles.box}>
           <img src="/images/trend_style/pic_a.jpg"></img>
         </div>
-        <div className={styles.box2}></div>
-        <div className={styles.box}></div>
-        <div className={styles.box}></div>
-        <div className={styles.box}></div>
+        <div className={styles.box}>
+          <img src="/images/style-5.jpg"></img>
+        </div>
+        <div className={styles.box}>
+          <img src="/images/trend_style/pic_f.jpg"></img>
+        </div>
         <div className={styles.box}></div>
         <div className={styles.box}></div>
         <div className={styles.box}></div>
