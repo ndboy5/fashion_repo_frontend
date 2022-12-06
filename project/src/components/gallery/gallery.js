daa8be8fde31ce0e38be3c85d49f08bd7f3efa2f
Yhujae <117284729+Yhujae@users.noreply.github.com>
2022-12-06 14:34:05 +0100

    fugui

--- project/src/components/gallery/gallery.js
+++ project/src/components/gallery/gallery.js
@@ -1,10 +1,8 @@
-import Header from "../layouts/header";
 import styles from "./gallery.module.css";
 function Gallery() {
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        <Header />
         <div className={styles.box1}>
           <img src="/images/trend_style/pic_a.jpg"></img>
         </div>
