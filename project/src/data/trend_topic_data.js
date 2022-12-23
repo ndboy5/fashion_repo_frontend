0f780aa034cf25165c4eae03240ad10082706b56
Nduka Okengwu <splashnds@yahoo.com>
2022-12-23 18:00:14 +0100

    Clicking on any topic now takes the user to the Community component

--- project/src/data/trend_topic_data.js
+++ project/src/data/trend_topic_data.js
@@ -5,49 +5,49 @@ const trending_topics_data = [
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 2,
     topic: "Gucci rollout massive trade deal in Nigeria",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 3,
     topic: "Top fabrics for Top-notch Business Men and Women",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 4,
     topic: "Fashion Book: How to make regular income with peace of mind",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 5,
     topic: "Pefume lover - A Fulani Woman's Diary",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 6,
     topic:
       "Turkey shirts Mark Spencer,farah Pant Trousers For 1500 Shirts,jeans And Chinos",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 7,
     topic: "Igbo Traditional Original George Rapper For Sale In Ibadan",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 8,
     topic:
       "2023 Stylish Ankara Asoebi Latest Styles | Beautiful Ways To Style Ankara",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 9,
     topic: "Fashion Design and Sweing Technology",
     link: "www.google.com",
   },
   {
-    id: 1,
+    id: 10,
     topic:
       "Christiano Ronaldo Agree to a $173 Million-per-year contract with Al Nasir",
     link: "www.google.com",
