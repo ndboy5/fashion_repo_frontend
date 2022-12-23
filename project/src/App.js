dd2e777c16d112e69afccdcfeb05487b7cd13f6c
Nduka Okengwu <splashnds@yahoo.com>
2022-12-23 17:53:00 +0100

    returned the header component to the App.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/layouts/header";
 import Footer from "./components/layouts/footer";
 import Nav from "./components/home/nav";
 import Home from "./components/home/home";
 import ChatContainer from "./components/chat/chatContainer";
 import Gallery from "./components/gallery/gallery";
 import OrderForm from "./components/order/orderForm";
+import Header from "./components/layouts/header";
 
 function App() {
   return (
     <>
       <div className="App">
+        <Header />
         <Nav />
         <Routes>
           {/*  defining the routes for the app  */}
           <Route path="/" element={<Home />} />
           <Route path="/community" element={<ChatContainer />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/topic/:id" element={<ChatContainer />} />
         </Routes>
         <Footer />
       </div>
--- project/src/components/home/topic.js
+++ project/src/components/home/topic.js
@@ -1,20 +1,23 @@
 // import trending_topics_data from "../../data/trend_topic_data";
+import { Link } from "react-router-dom";
 import styles from "./topic.module.css";
 
 function Topic(props) {
   // const topics = trending_topics_data;
   const topics = props.data;
-  console.log(props.message);
   return (
     <div className={styles.topic}>
       <div class="rows">
         <h4>TRENDING TOPICS</h4>
 
         {topics.map((tp) => (
-          <div className={styles.rows}>{tp.topic}</div>
+          <Link to={"/topic/" + tp.id} className={styles.rows}>
+            {tp.topic}
+          </Link>
         ))}
       </div>
     </div>
   );
 }
+
 export default Topic;
