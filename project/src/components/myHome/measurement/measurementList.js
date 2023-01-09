75ac41a4899f830fa6582ccccf80cdd107af1366
Nduka Okengwu <splashnds@yahoo.com>
2023-01-09 05:16:24 +0100

    I added sample data for measurements. The added data was for Male (Pattern and Free hand) and Female (Pattern only). Femal free hand remains outstanding

--- project/src/components/myHome/measurement/measurementList.js
+++ project/src/components/myHome/measurement/measurementList.js
@@ -1,5 +1,12 @@
 import measurementData from "../../../data/measurement_data";
-import { FaTrash, FaEdit } from "react-icons/fa";
+import {
+  FaTrash,
+  FaEdit,
+  FaFemale,
+  FaMale,
+  FaHandPaper,
+  FaDraftingCompass,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 /**
  * This component lists all the measurements in the user's records.
@@ -14,15 +21,14 @@ function MeasurementList() {
     <>
       <section>
         <button>New</button>
-      </section>
-      <section>
-        <button>New</button>
         <table>
           <tr>
             <th>SN</th>
             <th>Name</th>
             <th>Owner</th>
-            <th>Gender</th>
+            <th>Sex</th>
+            <th>Type</th>
+            <th>Measure Unit</th>
             <th>Last Update</th>
             <th> </th>
           </tr>
@@ -33,7 +39,17 @@ function MeasurementList() {
                 <Link to={"/myhome/measurement/" + m.id}>{m.name}</Link>
               </td>
               <td>{m.owner}</td>
-              <td>{m.gender}</td>
+              <td>
+                {m.gender === "M" ? <i>{<FaMale />}</i> : <i>{<FaFemale />}</i>}
+              </td>
+              <td>
+                {m.type === "Free Hand" ? (
+                  <i>{<FaHandPaper />}</i>
+                ) : (
+                  <i>{<FaDraftingCompass />}</i>
+                )}
+              </td>
+              <td>{m.unit}</td>
               <td>{m.last_update_date}</td>
               <td>
                 <i>
