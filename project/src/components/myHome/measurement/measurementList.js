46e243998bc19720f132396df7d6372e074a12f7
Nduka Okengwu <splashnds@yahoo.com>
2023-01-09 11:03:14 +0100

    added some CSS to the measurement List component'

--- project/src/components/myHome/measurement/measurementList.js
+++ project/src/components/myHome/measurement/measurementList.js
@@ -24,57 +24,67 @@ function MeasurementList() {
       <section>
         <button>New</button>
         <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Owner</th>
-            <th>Sex</th>
-            <th>Type</th>
-            <th>Measure Unit</th>
-            <th>
-              <FaHeart />{" "}
-            </th>
-            <th>Last Update</th>
-            <th></th>
-          </tr>
-          {myMeasurements.map((m, index) => (
+          <thead>
             <tr>
-              <td>{m.id}</td>
-              <td>
-                <Link to={"/myhome/measurement/" + index}>{m.name}</Link>
-              </td>
-              <td>{m.owner}</td>
-              <td>
-                {m.gender === "M" ? <i>{<FaMale />}</i> : <i>{<FaFemale />}</i>}
-              </td>
-              <td>
-                {m.type === "Free Hand" ? (
-                  <i alt="Free Hand">{<FaHandPaper />}</i>
-                ) : (
-                  <i alt="Pattern Design">{<FaDraftingCompass />}</i>
-                )}
-              </td>
-              <td>{m.unit}</td>
-              <td>
-                {m.is_favourite ? (
-                  <i>
-                    <FaHeart />
-                  </i>
-                ) : (
-                  " "
-                )}
-              </td>
-              <td>{m.last_update_date}</td>
-              <td>
-                <i>
-                  <FaTrash />
-                </i>{" "}
-                <i>
-                  <FaEdit />
-                </i>
-              </td>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Owner</th>
+              <th>Measure Unit</th>
+              <th>Last Update</th>
+              <th>Sex</th>
+              <th>Type</th>
+              <th>
+                <FaHeart />{" "}
+              </th>
+              <th></th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {myMeasurements.map((m, index) => (
+              <tr key={index}>
+                <td>{m.id}</td>
+                <td>
+                  <Link to={"/myhome/measurement/" + index}>{m.name}</Link>
+                </td>
+                <td>{m.owner}</td>
+
+                <td>{m.unit}</td>
+
+                <td>{m.last_update_date}</td>
+                <td>
+                  {m.gender === "M" ? (
+                    <i>{<FaMale />}</i>
+                  ) : (
+                    <i>{<FaFemale />}</i>
+                  )}
+                </td>
+                <td>
+                  {m.type === "Free Hand" ? (
+                    <i alt="Free Hand">{<FaHandPaper />}</i>
+                  ) : (
+                    <i alt="Pattern Design">{<FaDraftingCompass />}</i>
+                  )}
+                </td>
+                <td>
+                  {m.is_favourite ? (
+                    <i>
+                      <FaHeart />
+                    </i>
+                  ) : (
+                    " "
+                  )}
+                </td>
+                <td>
+                  <i>
+                    <FaTrash />
+                  </i>{" "}
+                  <i>
+                    <FaEdit />
+                  </i>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </section>
     </>
