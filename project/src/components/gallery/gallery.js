40b45e75d7f0e24e5da5780a364021b6758c43d2
Yhujae <117284729+Yhujae@users.noreply.github.com>
2023-01-25 04:01:30 +0100

    dfyugy

--- project/src/components/gallery/gallery.js
+++ project/src/components/gallery/gallery.js
@@ -1,161 +1,85 @@
 import styles from "./gallery.module.css";
-// import { HiArrowsExpand } from "react-icons/hi";
 import { BiExpand } from "react-icons/bi";
 import Modal from "../modal";
 import { useState } from "react";
+import galleryFilters from "../../data/galleryFilters_image_data";
 
 function Gallery() {
+  const [items, setItems] = useState(galleryFilters);
+  const filterItem = (categItem) => {
+    const updatedItems = galleryFilters.filter((galElem) => {
+      return galElem.category === categItem;
+    });
+    setItems(updatedItems);
+  };
   const [show, setShow] = useState(false);
+
   return (
     <section className={styles.container}>
-      <ul className={styles.imageGallery}>
-        <li>
-          <img src="/images/style-1.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button onClick={() => setShow(true)} className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <Modal onClose={() => setShow(false)} show={show} />
+      <section className={styles.filterHolder}>
+        <div className={styles.filters}>
+          <a
+            href="#"
+            onClick={() => setItems(galleryFilters)}
+            className={styles.a}
+          >
+            All
+          </a>
+        </div>
 
-        {/* <li>
-          <img src="/images/trend_style/pic_f.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_d.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_a.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_b.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_d.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_c.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_g.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_h.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/trend_style/pic_k.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/style-1.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="/images/style-2.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li>
+        <div className={styles.filters}>
+          <a
+            href="#"
+            onClick={() => filterItem("trousers")}
+            className={styles.a}
+          >
+            Trousers
+          </a>
+        </div>
 
-        <li>
-          <img src="/images/trend_style/pic_i.jpg" alt="" />
-          <div className={styles.overlay}>
-            <span>Image title</span>
-            <button className={styles.btn}>
-              <i className={styles.i}>
-                <BiExpand />
-              </i>
-            </button>
-          </div>
-        </li> */}
-      </ul>
+        <div className={styles.filters}>
+          <a
+            href="#"
+            onClick={() => filterItem("ankara styles")}
+            className={styles.a}
+          >
+            Ankara Styles
+          </a>
+        </div>
+
+        <div className={styles.filters}>
+          <a href="#" onClick={() => filterItem("skirts")} className={styles.a}>
+            Skirts
+          </a>
+        </div>
+
+        <div className={styles.filters}>
+          <a href="#" onClick={() => filterItem("gowns")} className={styles.a}>
+            Gowns
+          </a>
+        </div>
+      </section>
+
+      {items.map((elem) => {
+        const { id, image } = elem;
+
+        return (
+          <ul className={styles.imageGallery}>
+            <li>
+              <img src={image} alt="" />
+              <div className={styles.overlay}>
+                <span>Image title</span>
+                <button onClick={() => setShow(true)} className={styles.btn}>
+                  <i className={styles.i}>
+                    <BiExpand />
+                  </i>
+                </button>
+              </div>
+            </li>
+            <Modal onClose={() => setShow(false)} show={show} />
+          </ul>
+        );
+      })}
     </section>
   );
 }
---
+++ project/src/data/galleryFilters_image_data.js
@@ -0,0 +1,55 @@
+const galleryFilters = [
+  {
+    id: 1,
+    image: ["/images/style-3.jpg"],
+
+    category: "all",
+  },
+  {
+    id: 2,
+    image: ["/images/trend_style/pic_a.jpg"],
+
+    category: "trousers",
+  },
+
+  {
+    id: 3,
+    image: ["/images/style-1.jpg"],
+
+    category: "ankara styles",
+  },
+
+  {
+    id: 4,
+    image: ["/images/trend_style/pic_d.jpg"],
+
+    category: "skirts",
+  },
+
+  {
+    id: 5,
+    image: ["/images/trend_style/pic_e.jpg"],
+
+    category: "gowns",
+  },
+  {
+    id: 6,
+    image: ["/images/trend_style/pic_f.jpg"],
+
+    category: "ankara styles",
+  },
+  {
+    id: 7,
+    image: ["/images/trend_style/pic_g.jpg"],
+
+    category: "trousers",
+  },
+  {
+    id: 8,
+    image: ["/images/trend_style/pic_h.jpg"],
+
+    category: "skirts",
+  },
+];
+
+export default galleryFilters;
