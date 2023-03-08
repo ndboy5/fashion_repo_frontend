a9eabcce235a9119994d60aa045b09fd693ab353
Nduka Okengwu <splashnds@yahoo.com>
2023-03-08 16:33:58 +0100

    minor changes for home laptop

--- project/src/components/chat/chatContainer.js
+++ project/src/components/chat/chatContainer.js
@@ -5,8 +5,11 @@ import ChatHeader from "./chatHeader";
 import ChatMessage from "./chatMessage";
 import { useParams } from "react-router-dom";
 
+import socketIO from "socket.io-client";
+const socket = socketIO.connect("http://localhost:5000");
+
 function ChatContainer(props) {
-  let {id} = useParams();
+  let { id } = useParams();
   const [message, setMessage] = useState([]);
   function changeMessage(newMessage) {
     // event.preventDefault();
@@ -19,19 +22,17 @@ function ChatContainer(props) {
     //TODO: @Chima I beleive your intent is to do something with the message, you can do that here
   }
 
- 
-   // const { match } = props;
-    return (
-      <div className={styles.container}>
-        <p>Route params: {id}</p>
-        <ChatHeader />
-        <ChatBody message={message} />
-        <ChatMessage
-          onMessageChange={changeMessage}
-          onButtonClick={onButtonClick}
-        />
-      </div>
-    );
-  
+  // const { match } = props;
+  return (
+    <div className={styles.container}>
+      <p>Route params: {id}</p>
+      <ChatHeader />
+      <ChatBody message={message} />
+      <ChatMessage
+        onMessageChange={changeMessage}
+        onButtonClick={onButtonClick}
+      />
+    </div>
+  );
 }
 export default ChatContainer;
