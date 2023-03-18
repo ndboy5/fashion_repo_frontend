2486035416aa11c561195c5fb5174507e9c8c136
Yhujae <117284729+Yhujae@users.noreply.github.com>
2023-03-18 12:45:43 +0100

    deleted chatMessage

--- project/src/components/chat/chatContainer.js
+++ project/src/components/chat/chatContainer.js
@@ -5,7 +5,6 @@ import ChatHeader from "./chatHeader";
 import ChatMessage from "./chatMessage";
 import { useParams } from "react-router-dom";
 
-
 function ChatContainer(props) {
   let { id } = useParams();
   const [message, setMessage] = useState([]);
@@ -26,13 +25,6 @@ function ChatContainer(props) {
       <p>Route params: {id}</p>
       <ChatHeader />
       <ChatBody message={message} />
-<<<<<<< HEAD
-=======
-      <ChatMessage
-        onMessageChange={changeMessage}
-        onButtonClick={onButtonClick}
-      />
->>>>>>> a2793965e55c36527a6b8286591bb8e136ec07f7
     </div>
   );
 }
