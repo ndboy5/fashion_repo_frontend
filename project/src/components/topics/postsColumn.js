bb8a9196c8c809852543925b63ad677e1527640c
Nduka Okengwu <splashnds@yahoo.com>
2023-04-08 08:31:39 +0100

    Developed the PostColumn.js interface to add reactions

--- project/src/components/topics/postsColumn.js
+++ project/src/components/topics/postsColumn.js
@@ -2,7 +2,12 @@ import styles from "./postsColumn.module.css";
 import TopicPost from "./topicPost";
 
 function PostColumn({ posts }) {
-  return <div>{posts && posts.map((p) => <TopicPost text={p.text} />)}</div>;
+  // console.log(posts.length);
+  return (
+    <div className={styles.postColumn}>
+      {posts && posts.map((p) => <TopicPost key={p._id} text={p.text} />)}
+    </div>
+  );
 }
 
 export default PostColumn;
